f8d5b1e85b27c54d71833a9dfdf3f3a72beeb177
antonyorton <antony.orton@hotmail.com>
2025-07-05 22:37:24 +1000

    Added JMA certificate. Now no longer works on AWS lambda. Does work locally.

--- himawari-sharp.js
+++ himawari-sharp.js
@@ -7,9 +7,12 @@ const fs = require('fs')
 const fsPromises = fs.promises
 const path = require('path')
 const axios = require('axios')
+const https = require('https')
 
 async function download_images(imsize = '4d', savedir = './tmp/') {
-  console.log("Allowable 'imsize' input = 4d (small), 8d (medium) or 16d (large)")
+  console.log(
+    "Allowable 'imsize' input = 4d (small), 8d (medium) or 16d (large)"
+  )
   // const temp_savedir = './himawari_sharp_under_construction/images/'
   const temp_savedir = savedir
 
@@ -27,7 +30,8 @@ async function download_images(imsize = '4d', savedir = './tmp/') {
   const base_url = 'https://himawari8-dl.nict.go.jp/himawari8/img/D531106'
   // const url_base = [base_url, level, width, year, month, day, time].join('/')
 
-  const example_of_correct_uri = 'https://himawari8-dl.nict.go.jp/himawari8/img/D531106/8d/550/2024/06/27/025000_4_6.png'
+  const example_of_correct_uri =
+    'https://himawari8-dl.nict.go.jp/himawari8/img/D531106/8d/550/2024/06/27/025000_4_6.png'
   console.log('example_of_correct_uri:', example_of_correct_uri)
 
   console.log('base_url:', base_url)
@@ -46,7 +50,8 @@ async function download_images(imsize = '4d', savedir = './tmp/') {
 
   console.log('tiles:', tiles)
 
-  const date = await getLatestDate(base_url)
+  // const date = await getLatestDate(base_url)
+  const date = await get_my_date({ hrs_to_subtract: 1 }) // new update on 20250705 due to error with fetching of ..latest.json
   console.log('Himawari latest date is: ', date)
 
   let uri = ''
@@ -57,8 +62,13 @@ async function download_images(imsize = '4d', savedir = './tmp/') {
       uri = `${base_url}/${level}/${width}/${date}_${tile.x}_${tile.y}.png`
       // const uri = base_url + '/' + level + '/' + width + '/' + date + '_2_4.png'
       // console.log(uri)
-      tempdata = await asyncFetchImage(uri)
-      await asyncWriteImage(tempdata, `${temp_savedir}img_${level}_${tile.x}_${tile.y}.jpg`)
+
+      tempdata = await asyncFetchImage(uri, savedir)
+      // console.log('temp_data is: ', tempdata)
+      await asyncWriteImage(
+        tempdata,
+        `${temp_savedir}img_${level}_${tile.x}_${tile.y}.jpg`
+      )
     })
   ).catch(err => {
     console.log('Error in fetching of tiles ... ', err.message)
@@ -69,9 +79,30 @@ async function download_images(imsize = '4d', savedir = './tmp/') {
 // console.log('uri:', uri)
 
 //function to fetch the first image
-const asyncFetchImage = async uri => {
+const asyncFetchImage = async (uri, savedir) => {
+  // Need to configue different path format depending if in AWS Lambda or Local enviro
+
+  certlocation = './fullchain.pem' //Local format
+  // certlocation = '/var/task/fullchain.pem' //AWS Lambda format
+
   try {
-    const image = await axios.get(uri, { responseType: 'arraybuffer' })
+    //manually provide certificate info for the site (a bit of a mess around - copilot can help)
+    const caCert = await fsPromises.readFile(certlocation, 'utf8')
+    // console.log('caCert: ', caCert)
+    const httpsAgent = new https.Agent({
+      ca: caCert,
+      rejectUnauthorized: true
+    })
+
+    const image = await axios.get(uri, {
+      httpsAgent,
+      responseType: 'arraybuffer'
+    })
+
+    // const image = await axios.get(uri, {
+    //   responseType: 'arraybuffer'
+    // })
+
     return image.data
   } catch (error) {
     console.error('Error fetching image:', error.message)
@@ -91,6 +122,7 @@ const asyncWriteImage = async (data, filename) => {
 //function to get latest available datetime
 const getLatestDate = async base_url => {
   const request_url = base_url + '/latest.json'
+
   console.log('Request URL:', request_url)
   try {
     const response = await axios.get(request_url)
@@ -105,6 +137,64 @@ const getLatestDate = async base_url => {
   } catch (error) {
     console.error('Error fetching latest available datetime:', error.message)
   }
+}
+
+//function to get a recent UTC date rounding to nearest 10 min
+function get_my_date({ hrs_to_subtract = 0 } = {}) {
+  const now = new Date()
+
+  // Get UTC date
+  const [utcyear, utcmonth, utcday] = [
+    now.getUTCFullYear(),
+    now.getUTCMonth(),
+    now.getUTCDate()
+  ]
+  const [utchour, utcmin, utcsec] = [
+    now.getUTCHours(),
+    now.getUTCMinutes(),
+    now.getUTCSeconds()
+  ]
+  const nowUTC = new Date(utcyear, utcmonth, utcday, utchour, utcmin, utcsec)
+
+  let now_minused = new Date(nowUTC.valueOf())
+  //subtract 'hrs_to_subtract' hours
+  now_minused.setHours(now_minused.getHours() - hrs_to_subtract)
+  // round minutes to nearest 10 lower
+  let temp_minutes = now_minused.getMinutes()
+  now_minused.setMinutes(temp_minutes - (temp_minutes % 10))
+  //set seconds to zero
+  now_minused.setSeconds(0)
+  const dateLocalString = now.toString()
+  const dateString = now_minused.toLocaleDateString()
+  const timeString = now_minused.toLocaleTimeString()
+  // console.log(dateString)
+  console.log(timeString)
+  console.log(timeString.split(' ')[0].replaceAll(':', ''))
+
+  //get day, month, year and time
+  const [day, month, year] = dateString.split('/')
+  let time = timeString.split(' ')[0].replaceAll(':', '')
+  // make time 24hr format
+  const am_pm = timeString.split(' ')[1]
+  if (am_pm == 'pm') {
+    time = parseInt(time) + 120000
+    time = time.toString()
+  }
+
+  ;('add a zero to the front if required so the time always has six digits')
+  if (time.length < 6) {
+    time = '0' + time
+  }
+
+  // console.log('year: ', year)
+  // console.log('month: ', month)
+  // console.log('day: ', day)
+  // console.log('time: ', time)
+
+  //put in Himawari format
+  const output = `${year}/${month}/${day}/${time}`
+
+  return output
 }
 
 module.exports = { download_images }
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-//AWS lambda function - working (production) on 27 July 2024
+// UPDATE 2025-07-05 - This function no longer works on AWS Lambda
+// JMA have updated their certificates and so I needed to
+// download the full chain .pem files
+// and I havent yet got it running on AWS
+// Note that it does, however, work locally
+
+// PREVIOUS AWS lambda function - working (production) on 27 July 2024
 //payload to handler is of the form  { image_size: '16d', local_env: false } where image_size can be '4d', '8d', '16d'
 //and local_env must be false for AWS lambda environment. However, if running locally set local_env to true.
 //This is due to different path format on AWS lambda machines
@@ -7,7 +13,10 @@ const { create_composite } = require('./create-composite.js')
 
 async function handler(event) {
   try {
-    await create_composite((imsize = event.image_size), (local_env = event.local_env))
+    await create_composite(
+      (imsize = event.image_size),
+      (local_env = event.local_env)
+    )
     console.log('creation of himawari.jpg and push to S3 completed')
     const response = {
       statusCode: 200,
@@ -20,6 +29,6 @@ async function handler(event) {
 }
 
 // For testing on local environment set local_env: true. In AWS lamda, it is IMPORTANT that the event has a property local_env: false
-// handler({ image_size: '4d', local_env: true })
+handler({ image_size: '8d', local_env: true })
 
 module.exports = { handler }
